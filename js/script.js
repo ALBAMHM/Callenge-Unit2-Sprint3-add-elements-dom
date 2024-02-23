693a6d73e53fc76299ef98202688dd066cdf6b67
AlbaHoyo <alba.maria.hoyo@gmail.com>
2024-02-23 15:46:14 +0100

    V1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+
+const agregar= document.getElementById("agregar");
+const lista= document.getElementById("lista");
+
+
+agregar.addEventListener("click", function() {
+    let numero = prompt("Inserte número deseado :)");
+    let agregalista = document.createElement("li");
+    lista.appendChild(agregalista);
+    agregalista.textContent= numero;
+});
